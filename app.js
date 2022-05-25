e216c177899a91395492811dfb2b1653fe0777d7
juancastano0398 <juancastano0398@gmail.com>
2022-05-26 00:53:34 +0200

    Upload

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 //Import express library
 const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
 const { globalErrorHandler } = require('./controllers/errorController');
 const { postRouter } = require('./routes/repairRoutes');
 const { userRoutes } = require('./routes/userRoutes');
+dotenv.config({ path: './config.env' });
 
 //Create the server
 const app = express();
@@ -10,6 +15,22 @@ const app = express();
 //Enable incoming JSON data
 
 app.use(express.json());
+
+// Add security  helmets
+
+app.use(helmet());
+
+// Compress responses
+
+app.use(compression());
+
+// Log incoming requests
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+} else {
+  app.use(morgan('combined'));
+}
 
 //Endpoints
 
